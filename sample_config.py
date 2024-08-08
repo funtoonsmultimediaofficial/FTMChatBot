077d092c51dabd89a448d02bb45b0093b817edbd
funtoonsmultimediaofficial <funtoonsmultimedia@gmail.com>
2024-08-08 05:33:43 +0530

    Update sample_config.py

--- sample_config.py
+++ sample_config.py
@@ -10,8 +10,8 @@ if HEROKU:
 # NOTE Fill this if you are not deploying on heroku.
 if not HEROKU:
 
-    bot_token = "16901971:AAFqdM_SQE1PB2P1xLr67k"
-    ARQ_API_KEY = "Get this from @ARQRobot"
+    bot_token = "7377123347:AAENRXaAkigk4Ie2iQvqAdcBK1mmYaJL780"
+    ARQ_API_KEY = "NCQVZJ-OIZZUD-JKYSRR-JVGEMU-ARQ"
 # List of supported languages >>
 # https://py-googletrans.readthedocs.io/en/latest/#googletrans-languages
     LANGUAGE = "en"
